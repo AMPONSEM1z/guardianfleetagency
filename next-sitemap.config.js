6c95aad81d855ec25bdc0a63860fe8e18c300728
AMPONSEM1z <allbusiness1z1234@gmail.com>
2025-09-04 08:41:07 +0000

    site map build

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://guardianfleetagency.com", // ✅ your custom domain
+  generateRobotsTxt: true, // also creates robots.txt
+  sitemapSize: 7000, // split if too many URLs
+};
